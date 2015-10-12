92ee801ecbd3e1ab89f57288386d8de300c39301
Josue Valverde & Bunk Master Flash <student@epicodus.com>
2015-10-12 11:23:31 -0700

    add methods to RegionsController

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -4,4 +4,39 @@ class RegionsController < ApplicationController
     render :index
   end
 
+  def show
+    @region = Region.find(params[:id])
+    render :show
+  end
+
+  def new
+    @region = Region.new
+    render :new
+  end
+
+  def create
+    @region = Region.new(region_params)
+    if @region.save
+      redirect_to regions_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @region = Region.find(params[:id])
+    render :edit
+  end
+
+  def destroy
+    @region = Region.find(params[:id])
+    @region.destroy
+    redirect_to regions_path
+  end
+
+private
+  def region_params
+    params.require(:region).permit(:name)
+  end
+  
 end
